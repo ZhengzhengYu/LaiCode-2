84583620c058a0c43145bcb5905bfe78497be38a
xinyuw <xinyuwang@xinyuwdeMBP.lan1>
2019-01-29 22:25:42 -0800

    QuickSelect

--- Heap/src/LaiCodeKSmallestInUnsortedArray.java
+++ Heap/src/LaiCodeKSmallestInUnsortedArray.java
@@ -25,27 +25,83 @@ public class LaiCodeKSmallestInUnsortedArray {
 
     //Method 2
     //max heap
+//    public class Solution {
+//        public int[] kSmallest(int[] array, int k) {
+//            int[] result = new int[k];
+//            if (array == null || array.length == 0 || k == 0) {
+//                return result;
+//            }
+//            Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+//            for (int i = 0; i < k; i++) {
+//                pq.offer(array[i]);
+//            }
+//            for (int i = k; i < array.length; i++) {
+//                if (array[i] <= pq.peek()) {
+//                    pq.poll();
+//                    pq.offer(array[i]);
+//                }
+//            }
+//            for (int i = k; i > 0; i--) {
+//                result[i - 1] = pq.poll();
+//            }
+//            return result;
+//        }
+//    }
+
+
+
+    // Method 3
+    //QuickSelect: QuickSort
+    //选择一个index，通过QuickSort，左边的都比index小，右边的都比index大
+    //Time Complexity: O(n) on average; O(n^2) worse case
+    //Space Complexity: O(log n) on average; O(n) worse case
     public class Solution {
         public int[] kSmallest(int[] array, int k) {
             int[] result = new int[k];
             if (array == null || array.length == 0 || k == 0) {
                 return result;
             }
-            Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            QuickSelect(array, k, 0, array.length - 1);
             for (int i = 0; i < k; i++) {
-                pq.offer(array[i]);
+                result[i] = array[i];
             }
-            for (int i = k; i < array.length; i++) {
-                if (array[i] <= pq.peek()) {
-                    pq.poll();
-                    pq.offer(array[i]);
-                }
-            }
-            for (int i = k; i > 0; i--) {
-                result[i - 1] = pq.poll();
-            }
+            Arrays.sort(result);
             return result;
         }
+
+        private void QuickSelect(int[] array, int k, int left, int right) {
+            if (left >= right) {
+                return;
+            }
+            Random random = new Random();
+            int pivotIndex = left + random.nextInt(right - left + 1);
+            //put the pivotIndex at the right at first
+            swap(array, pivotIndex, right);
+            int l = left;
+            int r = right - 1;
+            while (l <= r) {
+                if (array[l] < array[right]) {
+                    l++;
+                } else {
+                    swap(array, l, r);
+                    r--;
+                }
+            }
+            //put int pivotIndex at the right place, left small than pivotIndex, right big than pivotIndex
+            swap(array, l, right);
+            if (k < l) {
+                QuickSelect(array, k, left, l - 1);
+            } else {
+                QuickSelect(array, k, l + 1, right);
+            }
+        }
+
+        private void swap(int[] array, int i, int j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
     }
 
 }
